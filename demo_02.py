a81fd920dcf45b90ebdfd3f5e002021ab2313ba9
Naohiro2g <587586+Naohiro2g@users.noreply.github.com>
2024-05-16 14:05:43 +0900

    demo_02 to demo_03

--- demo_02.py
+++ demo_02.py
@@ -4,7 +4,6 @@
 from datetime import datetime
 import pygame
 from seven_seg_pg import Seven_seg
-from lcd_font_pg import LCD_font
 
 
 DARK_GRAY = (40, 40, 40)
@@ -18,37 +17,29 @@ WHITE = (250, 250, 250)
 pygame.init()
 
 clock = pygame.time.Clock()
-screen = pygame.display.set_mode([480, 320])
+screen = pygame.display.set_mode([400, 320])
 pygame.display.set_caption("pygame 7-segment display simulation")
 screen.fill(DARK_GRAY)
 
-# display1 = Seven_seg(screen)
-# display1.init_col(BLOCK_SIZE=9, BLOCK_INTV=10, COLOR_ON=GREEN, COLOR_OFF=DARK_GRAY)
-# display1.init_row(X_ORG=8, Y_ORG=22, COL_INTV=6)
+display1 = Seven_seg(screen)
+display1.init_col(BLOCK_SIZE=9, BLOCK_INTV=10, COLOR_ON=GREEN, COLOR_OFF=DARK_GRAY)
+display1.init_row(X_ORG=8, Y_ORG=22, COL_INTV=6)
 
-# display2 = Seven_seg(screen)
-# display2.init_col(BLOCK_SIZE=7, BLOCK_INTV=8, COLOR_ON=RED, COLOR_OFF=GRAY)
-# display2.init_row(X_ORG=2, Y_ORG=18, COL_INTV=6)
+display2 = Seven_seg(screen)
+display2.init_col(BLOCK_SIZE=7, BLOCK_INTV=8, COLOR_ON=RED, COLOR_OFF=GRAY)
+display2.init_row(X_ORG=2, Y_ORG=18, COL_INTV=6)
 
-# display3 = Seven_seg(screen)
-# display3.init_col(BLOCK_SIZE=4, BLOCK_INTV=4)
-# display3.init_row(X_ORG=20, Y_ORG=66, COL_INTV=6)
+display3 = Seven_seg(screen)
+display3.init_col(BLOCK_SIZE=4, BLOCK_INTV=4)
+display3.init_row(X_ORG=20, Y_ORG=66, COL_INTV=6)
 
-# display4 = Seven_seg(screen)
-# display4.init_col(BLOCK_SIZE=4, BLOCK_INTV=4)
-# display4.init_row(X_ORG=2, Y_ORG=76, COL_INTV=6)
+display4 = Seven_seg(screen)
+display4.init_col(BLOCK_SIZE=4, BLOCK_INTV=4)
+display4.init_row(X_ORG=2, Y_ORG=76, COL_INTV=6)
 
 display5 = Seven_seg(screen)
 display5.init_col(BLOCK_SIZE=9, BLOCK_INTV=9, COLOR_ON=(120, 200, 250), COLOR_OFF=GRAY)
 display5.init_row(X_ORG=8, Y_ORG=8, COL_INTV=6)
-
-display6 = LCD_font(screen)
-display6.init_col(BLOCK_SIZE=4, BLOCK_INTV=6, COLOR_ON=WHITE, COLOR_OFF=GRAY)
-display6.init_row(X_ORG=10, Y_ORG=21, COL_INTV=6)
-
-display7 = LCD_font(screen)
-display7.init_col(BLOCK_SIZE=6, BLOCK_INTV=8, COLOR_ON=RED, COLOR_OFF=GRAY)
-display7.init_row(X_ORG=6, Y_ORG=24, COL_INTV=6)
 
 
 running = True
@@ -63,50 +54,26 @@ while running:
             break
         # 「for count」のループから抜ける。whileループも抜ける。
 
-        # display1.update_col(col=0, num=count // (16 ** 3))   # 4096の位
-        # display1.update_col(col=1, num=count // (16 ** 2))   # 256の位
-        # display1.update_col(col=2, num=count // 16)          # 16の位
-        # display1.update_col(col=3, num=count)                # 1の位
+        display1.update_col(col=0, num=count // (16 ** 3))   # 4096の位
+        display1.update_col(col=1, num=count // (16 ** 2))   # 256の位
+        display1.update_col(col=2, num=count // 16)          # 16の位
+        display1.update_col(col=3, num=count)                # 1の位
 
-        # display2.update_col(col=0, num=count // (10 ** 4), base=10)   # 1000の位
-        # display2.update_col(col=1, num=count // (10 ** 3), base=10)   # 1000の位
-        # display2.update_col(col=2, num=count // (10 ** 2), base=10)   # 100の位
-        # display2.update_col(col=3, num=count // (10 ** 1), base=10)   # 10の位
-        # display2.update_col(col=4, num=count // (10 ** 0), base=10)   # 1の位
+        display2.update_col(col=0, num=count // (10 ** 4), base=10)   # 1000の位
+        display2.update_col(col=1, num=count // (10 ** 3), base=10)   # 1000の位
+        display2.update_col(col=2, num=count // (10 ** 2), base=10)   # 100の位
+        display2.update_col(col=3, num=count // (10 ** 1), base=10)   # 10の位
+        display2.update_col(col=4, num=count // (10 ** 0), base=10)   # 1の位
 
-        # display3.disp_num2(zfil=False, rjust=3, num=count, base=10)
+        display3.disp_num2(zfil=False, rjust=3, num=count, base=10)
 
-        # display4.disp_num2(zfil=True, rjust=16, num=count, base=2)
+        display4.disp_num2(zfil=True, rjust=16, num=count, base=2)
 
         dt_now = datetime.now()
         time_now = (dt_now.hour * 10000
                     + dt_now.minute * 100
                     + dt_now.second)
         display5.disp_num2(zfil=True, rjust=6, num=time_now, base=10)
-
-        display6.update_col(col=0, code=int(str(dt_now.year)[0]))
-        display6.update_col(col=1, code=int(str(dt_now.year)[1]))
-        display6.update_col(col=2, code=int(str(dt_now.year)[2]))
-        display6.update_col(col=3, code=int(str(dt_now.year)[3]))
-        display6.update_col(col=4, code=11)
-        display6.update_col(col=5, code=dt_now.month // 10)
-        display6.update_col(col=6, code=dt_now.month % 10)
-        display6.update_col(col=7, code=11)
-        display6.update_col(col=8, code=dt_now.day // 10)
-        display6.update_col(col=9, code=dt_now.day % 10)
-
-        display7.update_col(col=0, code=dt_now.hour // 10)
-        display7.update_col(col=1, code=dt_now.hour % 10)
-        display7.update_col(col=2, code=10)
-        display7.update_col(col=3, code=dt_now.minute // 10)
-        display7.update_col(col=4, code=dt_now.minute % 10)
-        display7.update_col(col=5, code=10)
-        display7.update_col(col=6, code=dt_now.second // 10)
-        display7.update_col(col=7, code=dt_now.second % 10)
-
-
-
-
 
         pygame.display.flip()  # update_col
         clock.tick(20)  # FPS, Frame Per Second
